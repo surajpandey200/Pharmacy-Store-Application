e2013e40021ad21f4c29c5bf9d9dfdb8678cf7cd
surajpandey200 <73788819+surajpandey200@users.noreply.github.com>
2022-05-23 11:53:16 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connection SUCCESS");
+  } catch (error) {
+    console.error("MongoDB connection FAIL");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
